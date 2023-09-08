fba49fa1581178f0c2de0798ddb394172a2279ed
Diazkaka22 <126131717+Diazkaka22@users.noreply.github.com>
2023-09-08 19:50:42 +0700

    Update profile.php

--- phpmvc/app/view/User/profile.php
+++ phpmvc/app/view/User/profile.php
@@ -1,5 +1,5 @@
 <div class="container text-center mt-4">
-    <h1>Halaman Creator</h1>
+    <h1>Halaman Profil</h1>
     <img src="http://localhost/phpmvc/img/kaka.jpg" alt="kaka" width="200" class="rounded-circle shadow mb-2">
     <p>Hallo, Nama saya <?= $data['nama']?>, Saya seorang <?=$data['pekerjaan']?></p>
 
