c4bfe064304d3d9429cd3cd9b62cc8ff33743570
Diazkaka22 <126131717+Diazkaka22@users.noreply.github.com>
2023-09-08 19:52:58 +0700

    Update index.php

--- phpmvc/app/view/Home/index.php
+++ phpmvc/app/view/Home/index.php
@@ -1,20 +1,111 @@
 <style>
-   body {
-    background-image: url(http://localhost/phpmvc/img/lp.jpg);
-   }
-   
-  
-   .jumbotron {
-    opacity: 0.9;
+    {
+    }
+* {
+	padding:0;
+	margin:0;
+	font-family: sans-serif;
+}
+a {
+	color: inherit;
+	text-decoration: none;
+}
+.medsos {
+	padding:5px 0;
+	background-color: #148F77;
+}
+.medsos ul li {
+	display: inline-block;
+	color: #fff;
+	margin-right: 10px;
+}
+.container {
+	width:30%;
+	margin:auto;
+}
+.container:after {
+	content:'';
+	display: block;
+	clear: both;
+}
+header h1 {
+	float: left;
+	padding:15px 0;
+	color: #148F77;
+}
+header ul {
+	float: right;
+}
+header ul li {
+	display: inline-block;
+}
+header ul li a {
+	padding:25px 20px;
+	display: inline-block;
+}
+header ul li a:hover {
+	background-color: #148F77;
+	color: #fff;
+}
+.banner {
+	height: 60vh;
+	background-image: url('../img/lp.jpg');
+	background-size: cover;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.banner:after {
+	content:'';
+	display: block;
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: rgba(20, 143, 119, .6);
+}
+.banner h2 {
+	color: #fff;
+	z-index: 1;
+	padding: 20px 25px;
+	border:3px solid #fff;
+}
+section {
+	padding:50px 0;
+}
+section h3 {
+	text-align: center;
+	padding:20px 0;
+	color: #148F77;
+	margin-bottom: 25px;
 }
 
-
 </style>
-<div class="container">
-    <div class="jumbotron mt-4">
-        <h1 class="display-4" style="color: ;"> Heloo, Welcome to Smkn 2 Trenggalek</h1>
-        <hr class="my-4">
-        <p>Sekolah Menengah Kejuruan Negeri 2 Trenggalek</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
-    </div>
-</div>
+<body>
+
+
+	<!-- header -->
+	<div class="medsos">
+		<div class="container">
+			<ul>
+				<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+			</ul>
+		</div>
+	</div>
+	<header>
+		<div class="container">
+			<h1>
+          <a>Software engineering</a></h1>      
+		</div>
+	</header>
+
+	<!-- banner -->
+	<section class="banner">
+		<h2>Halo Selamat datang di SMKN 2 TRENGGALEK</h2>
+
+	</section>
+	</section>
