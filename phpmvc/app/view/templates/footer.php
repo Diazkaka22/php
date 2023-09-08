bedc5a69bfaddacc3549f95317858e3b8c83a1b4
Diazkaka22 <126131717+Diazkaka22@users.noreply.github.com>
2023-09-08 14:32:28 +0700

    Update footer.php

--- phpmvc/app/view/templates/footer.php
+++ phpmvc/app/view/templates/footer.php
@@ -52,7 +52,7 @@
 
   <!-- Copyright -->
   <div class="footer-copyright text-center py-3">© 2023 Copyright:
-    <a href="/"> By Diaz Ibanez Kaka</a>
+    <a href="#"> By Diaz Ibanez Kaka</a>
   </div>
   <!-- Copyright -->
 
